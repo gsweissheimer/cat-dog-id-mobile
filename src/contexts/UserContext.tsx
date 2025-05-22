dedbe1c0b5f1b1803f9e9df30e068e4b0a4cf1f5
Guilherme Weissheimer <gweissheimer@tech6group.com>
2025-05-22 08:13:08 -0300

    fix: improve user token handling and refine userPets state type

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,23 +1,23 @@
 import React, { createContext, useContext, useState, ReactNode } from 'react';
 import { getUserFullFromUserId } from '../services/api';
 import { useAuth } from './AuthContext';
-import { AnswerDTO, UserFull } from '../types/';
+import { AnswerDTO, UserFull, Pet } from '../types/';
 
 type UserContextData = {
   userFull: UserFull | null;
   setUserFull: React.Dispatch<React.SetStateAction<UserFull | null>>;
   getUserFull: () => Promise<boolean>;
-  userPets: any[];
-  setUserPets: React.Dispatch<React.SetStateAction<any[]>>;
+  userPets: Pet[];
+  setUserPets: React.Dispatch<React.SetStateAction<Pet[]>>;
 };
 
 const UserContext = createContext<UserContextData>({} as UserContextData);
 
 export function UserProvider({ children }: { children: ReactNode }) {
   const [userFull, setUserFull] = useState<UserFull | null>(null);
-  const [userPets, setUserPets] = useState<any[]>([]);
+  const [userPets, setUserPets] = useState<Pet[]>([]);
   const { getUserIdFromToken } = useAuth();
-
+  
   async function getUserFull(): Promise<boolean> {
     const userId: string = getUserIdFromToken();
     const result: AnswerDTO<UserFull> = await getUserFullFromUserId(userId);
@@ -26,9 +26,8 @@ export function UserProvider({ children }: { children: ReactNode }) {
     }
     const petsList = [
         ...(result.data.pets || []),
-        ...(result.data.family?.pets || [])
+        ...(result.data.family?.users[0].pets || [])
     ];
-    console.log('petsList', petsList);
     setUserFull(result.data);
     setUserPets(petsList);
     return true;
